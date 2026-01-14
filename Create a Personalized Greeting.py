ace49f72443bd214cfc1791d61fd904e54ea7627
Krishnendu gorai <krishnendugorai17@gmail.com>
2026-01-14 20:24:12 +0530

    initial commit

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,6 @@
+
+first_name = str(input('Enter first name:'))
+second_name = str(input('Enter second name:'))
+
+#now we will print and put the variable value
+print('Hello', first_name, second_name, '!', 'Welcome to the python program', )
